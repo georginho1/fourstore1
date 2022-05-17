0465ce21c05f47c937c967a0c40e9d45818ca62b
isoares <isoares@os2h-alp-not213.server01.foursys.local>
2022-05-17 10:25:34 -0300

    Instanceando a classe menu na aplication.

--- src/br/com/foursys/fourcamp/fourstore/Application.java
+++ src/br/com/foursys/fourcamp/fourstore/Application.java
@@ -1,9 +1,13 @@
 package br.com.foursys.fourcamp.fourstore;
 
+import br.com.foursys.fourcamp.fourstore.communication.MainMenu;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
+		MainMenu menu = new MainMenu();
+		
+		menu.mainMenu();
 
 	}
 
