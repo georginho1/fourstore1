d788b699d3c4adf336f72392a5943f7e7f404cfd
isoares <isoares@os2h-alp-not213.server01.foursys.local>
2022-05-20 11:57:04 -0300

    Merge remote-tracking branch 'origin/main' into main-criarmenu-20

--- src/br/com/foursys/fourcamp/fourstore/communication/MainMenu.java
+++ src/br/com/foursys/fourcamp/fourstore/communication/MainMenu.java
@@ -9,17 +9,18 @@ public class MainMenu {
 		primaryMenu();
 
 	}
+	
 	private void primaryMenu() {
 		Scanner scanner = new Scanner(System.in);
 		int option = -1;
 		
 		while(option != 0) {
 			System.out.println("==========FOURSTORE=============||");
-			System.out.println("1 - Clientes                    ||");
-			System.out.println("2 - Produtos                    ||");
-			System.out.println("3 - Vendas                      ||");
+			System.out.println("1 - Produtos                    ||");
+			System.out.println("2 - Vendas                      ||");
+//			System.out.println("3 - Clientes                    ||");
 			System.out.println("0 - Sair do sistema             ||");
-			System.out.print("Insira uma op√ß√£o: ");
+			System.out.print("Insira uma opÁ„o: ");
 			option = scanner.nextInt();
 			System.out.println("----------------------------------\n");
 
@@ -29,16 +30,16 @@ public class MainMenu {
 					System.out.println("\nSistema encerrado");
 					break;
 				case 1:
-					this.menuClients();
-					break;
-				case 2:
 					this.menuProducts();
 					break;
-				case 3:
+				case 2:
 					this.menuSales();
 					break;
+//				case 3:
+//					this.menuClients();
+//					break;
 				default:
-					System.out.println("\nOp√ß√£o inv√°lida. Tente novamente.\n");
+					System.out.println("\nOpÁ„o invalida. Tente novamente.\n");
 			}
 		}
 	}
@@ -53,21 +54,21 @@ public class MainMenu {
 			Scanner sc = new Scanner(System.in);
 			option = sc.nextInt();
 			switch(option) {
-			case 0:{
-				primaryMenu();
-				break;
+				case 0:{
+					primaryMenu();
+					break;
+				}
+				case 1:{
+				//	saleConsultation(); metodo para consultar venda
+					break;
+				}
+				case 2:{
+				//saleRegister(); metodo para realizar venda
+					break;
+				}
+				default:
+					System.out.println("\nOpÁ„o invalida. Tente Novamente. \n");
 			}
-			case 1:{
-			//	saleConsultation(); metodo para consultar venda
-				break;
-			}
-			case 2:{
-			//saleRegister(); metodo para realizar venda
-				break;
-			}
-			default:
-				System.out.println("\nOpc√£o Inv√°lida. Tente Novamente. \n");
-		}
 		}
 		
 	}
@@ -76,57 +77,57 @@ public class MainMenu {
 		int option = -1;
 		
 		while(option != 0) {
-		System.out.println("1 - Cadastrar Produto"
-				+ "\n2 - Buscar Produto (ID)"
-				+ "\n3 - Buscar Produto (SKU)"
-				+ "\n4 - Lista Produtos"
-				+ "\n5 - Atualizar Produtos"
-				+ "\n6 - Excluir Produto"
-				+ "\n0 - Para voltar");
-		
-		Scanner sc = new Scanner(System.in);
-		option = sc.nextInt();
-		
-		switch(option) {
-		case 0:{
-			primaryMenu();
-			break;
+			System.out.println("1 - Cadastrar Produto"
+					+ "\n2 - Buscar Produto (ID)"
+					+ "\n3 - Buscar Produto (SKU)"
+					+ "\n4 - Lista Produtos"
+					+ "\n5 - Atualizar Produtos"
+					+ "\n6 - Excluir Produto"
+					+ "\n0 - Para voltar");
+			
+			Scanner sc = new Scanner(System.in);
+			option = sc.nextInt();
+			
+			switch(option) {
+				case 0:{
+					primaryMenu();
+					break;
+				}
+				case 1:{
+					//cadastrarProduto(); metodo para cadastrar produto
+					break;
+				}
+				case 2:{
+					//buscarProdutoId(); metodo para buscar produto pelo ID
+					break;
+				}
+				case 3:{
+					//buscarProdutoSku(); metodo para buscar pelo SKU
+					break;
+				}
+				case 4:{
+					//listarProdutos(); metodo para listar produtos
+					break;
+				}
+				case 5:{
+					//atualizarProdutos(); metodo para atualizar produtos
+					break;
+				}
+				case 6:{
+					//excluirProdutos(); metodo para excluir produtos
+					break;
+				}
+				default:
+					System.out.println("\nOpÁ„o invalida. Tente Novamente \n");
+			}
+			
 		}
-		case 1:{
-			//cadastrarProduto(); metodo para cadastrar produto
-			break;
-		}
-		case 2:{
-			//buscarProdutoId(); metodo para buscar produto pelo ID
-			break;
-		}
-		case 3:{
-			//buscarProdutoSku(); metodo para buscar pelo SKU
-			break;
-		}
-		case 4:{
-			//listarProdutos(); metodo para listar produtos
-			break;
-		}
-		case 5:{
-			//atualizarProdutos(); metodo para atualizar produtos
-			break;
-		}
-		case 6:{
-			//excluirProdutos(); metodo para excluir produtos
-			break;
-		}
-		default:
-			System.out.println("\nOp√ß√£o Invalida. Tente Novamente \n");
-		}
-		
-	}
 	}
 
-	private void menuClients() {
-		
-		
-	}
-		
-	}
+//	private void menuClients() {
+//		
+//		
+//	}
+//		
+}
 
