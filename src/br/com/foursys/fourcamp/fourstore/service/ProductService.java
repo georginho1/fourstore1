5fd798df438739895c54f64269b434d8e3b4ac15
rsimeoni <rsimeoni@wgod-alp-not257.server01.foursys.local>
2022-05-24 20:11:34 -0300

    Criação do método haveStock em ProductService.java

--- src/br/com/foursys/fourcamp/fourstore/service/ProductService.java
+++ src/br/com/foursys/fourcamp/fourstore/service/ProductService.java
@@ -1,6 +1,24 @@
 package br.com.foursys.fourcamp.fourstore.service;
 
+import br.com.foursys.fourcamp.fourstore.data.ProductData;
+import br.com.foursys.fourcamp.fourstore.model.Product;
 
 public class ProductService {
+	
+	ProductData data = new ProductData();
+	
+	public Boolean haveStock(Product purchaseProduct, Integer purchaseQuantity) {
+		String purchaseProductId = purchaseProduct.getId();
+		Product productInStock = data.getProductById(purchaseProductId);
+		Integer quantityInStock = productInStock.getQuantity();
+		
+		if(productInStock != null) {
+			if(quantityInStock >= purchaseQuantity) {
+				return true;
+			}
+		}
 
+		return false;
+		
+	}
 }
